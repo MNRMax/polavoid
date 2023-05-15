da6f471d0802a54a9540b5e845d82ad7b76b09ba
melinamui <113132371+melinamui@users.noreply.github.com>
2023-05-15 13:54:14 -0400

    i think i made the photo look better

--- vite-project/src/components/postItem.vue
+++ vite-project/src/components/postItem.vue
@@ -1,67 +1,79 @@
 <script setup>
-import ProfilePicture from './ProfilePicture.vue';
-import { supabase } from '../supabase'
-import { ref, onMounted } from 'vue';
-import '../assets/variables.css'
+import ProfilePicture from "./ProfilePicture.vue";
+import { supabase } from "../supabase";
+import { ref, onMounted } from "vue";
+import "../assets/main.css";
 
 const props = defineProps({
-    post: Object,
-})
+  post: Object,
+});
 
-const profile = ref(undefined)
+const profile = ref(undefined);
 
 async function getProfile() {
-    console.log(props.post.author)
-    const { data, error } = await supabase
-        .from('profiles')
-        .select()
-        .eq('id', props.post.author)
-    profile.value = data[0]
+  console.log(props.post.author);
+  const { data, error } = await supabase
+    .from("profiles")
+    .select()
+    .eq("id", props.post.author);
+  profile.value = data[0];
 }
 // getProfile().then(data => {
 //     profile.value = data;
 // })
 onMounted(() => {
-    getProfile()
-})
+  getProfile();
+});
 </script>
 
 <template>
-    <div v-if="profile" id="post">
-        <img id="postImage" src="https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-10.jpg?ssl=1">
-        <div id="bottom">
-            <ProfilePicture id="pfp" :src="profile.avatar_url" />
-            <p id="signature">{{ profile.username }}</p>
-            <p id="signature">{{ props.post.caption }}</p>
-        </div>
+  <div v-if="profile" id="post">
+    <img
+      id="postImage"
+      src="https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-10.jpg?ssl=1"
+    />
+    <div id="bottom">
+      <h1 id="caption">{{ props.post.caption }}</h1>
+      <div id="user">
+        <ProfilePicture id="pfp" :src="profile.avatar_url" />
+        <h2 id="signature">{{ profile.username }}</h2>
+      </div>
     </div>
+  </div>
 </template>
 
 <style>
 #post {
-    margin: auto;
-    width: 40%;
-    background-color: aliceblue;
-    box-shadow: 10px 10px 10px black;
-    padding: 1.5rem;
+  margin: auto;
+  width: 40%;
+  background-color: aliceblue;
+  box-shadow: 20px 10px 10px black;
+  padding: 1.5rem 1.5rem 0.2rem 1.5rem;
 }
 
 #postImage {
-    width: 100%;
+  width: 100%;
 }
 
 #pfp {
-    width: 5rem;
-    height: 5rem;
-    padding: 10px;
+  width: 4rem;
+  height: 4rem;
+  padding: 10px;
 }
-#bottom {
-    display: flex;
+
+#user {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
+
 #signature {
-    margin-left: 4rem;
-    font-size: 2rem;
-    color: black;
-    font-family: 'Caveat', cursive;
+  color: black;
+  font-family: "Caveat", cursive;
+}
+
+#caption {
+  color: black;
+  font-family: "Caveat", cursive;
 }
 </style>
