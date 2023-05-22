b3770ec74f685e0c6aecdc04ef09886fae6a7d64
MNRMax <maximilian.shabatayev@gmail.com>
2023-05-22 09:23:38 -0400

    made the card flip

--- vite-project/src/components/postItem.vue
+++ vite-project/src/components/postItem.vue
@@ -9,6 +9,7 @@ const props = defineProps({
 });
 
 const profile = ref(undefined);
+const flipped = ref(false);
 
 async function getProfile() {
   console.log(props.post.author);
@@ -27,7 +28,9 @@ onMounted(() => {
 </script>
 
 <template>
-    <div v-if="profile" id="post">
+  <div v-if="profile" id="post" @click="flipped = !flipped">
+    <div id="inner" :class="flipped? 'flipped': ''">
+      <div id="front">
         <img v-if="props.post.image" id="postImage" :src="props.post.image">
         <img v-else id="postImage" src="noupload.png">
         <h2 id="caption">{{ props.post.caption }}</h2>
@@ -37,19 +40,43 @@ onMounted(() => {
             <h2 id="signature">{{ profile.username }}</h2>
           </div>
         </div>
+      </div>
+      <div id="back">
+        <h2>h idsufdhasuhdfuhisu hifu hasui hfdih suaihfui hisdhiuaf hiusdhf uhaufh</h2>
+      </div>
     </div>
+  </div>
 </template>
 
 <style>
 #post {
+  /* perspective: 1000px; */
+  width: 20%;
+}
+#front, #back {
+  height: 54rem;
+  position: absolute;
   margin: auto;
-  width: 40%;
+  width: 100%;
   background-color: aliceblue;
   box-shadow: 20px 20px 20px rgb(106, 45, 30);
   padding: 1.5rem 1.5rem 0.2rem 1.5rem;
   overflow-wrap: break-word;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
 }
-
+#back {
+  transform: rotateY(180deg);
+}
+.flipped {
+  transform: rotateY(180deg);
+}
+#inner {
+  position: relative;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+  transform-origin: center center;
+}
 #postImage {
   width: 100%;
 }
