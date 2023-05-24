759b35f46a41127a5f9160577779c5ba6b30a975
MNRMax <maximilian.shabatayev@gmail.com>
2023-05-24 09:57:18 -0400

    people menu

---
+++ vite-project/src/components/PersonItem.vue
@@ -0,0 +1,53 @@
+<script setup>
+import ProfilePicture from "./ProfilePicture.vue"
+import FollowButton from "./followButton.vue";
+import { useSessionStore } from "../stores/session";
+
+const sessionStore = useSessionStore();
+const props = defineProps({
+    user: Object
+})
+</script>
+
+<template>
+    <div v-if="props.user" id="card">
+        <ProfilePicture id="pfp" :src="props.user.avatar_url"/>
+        <div>
+            <h2>{{ props.user.username }}</h2>
+            <h3>{{ props.user.full_name }}</h3>
+        </div>
+        <FollowButton v-if="props.user.id !== sessionStore.session.value.user.id" id="followButton" :followed-user="props.user.id"/>
+    </div>
+</template>
+
+<style scoped>
+#card {
+    position: relative;
+    background-color: rgb(44, 44, 44);
+    padding: 2rem;
+    display: flex;
+    box-shadow: 20px 20px 20px rgb(106, 45, 30);
+    border: 2px black solid;
+    border-radius: 10px;
+    margin-bottom: 2rem;
+}
+
+h2 {
+    margin: 0;
+}
+
+h3 {
+    margin: 0;
+}
+
+#pfp {
+    height: 4rem;
+    margin-right: 3rem;
+}
+#followButton {
+    position: absolute; 
+    right: 10%;
+    top: 50%;
+    transform: translateY(-50%);
+}
+</style>
---
+++ vite-project/src/views/PeopleView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue"
+import PersonItem from "../components/PersonItem.vue"
+import { supabase } from "../supabase";
+
+const users = ref(null)
+
+function redirect(id) {
+    window.location = "/profile/" + id
+}
+
+async function getAllUsers() {
+    const { data, error } = await supabase
+        .from("profiles")
+        .select()
+    users.value = data;
+    console.log(data)
+}
+
+getAllUsers()
+</script>
+
+<template>
+    <PersonItem v-if="users" v-for="user in users" :user="user" @click="redirect(user.id)"/>
+</template>
