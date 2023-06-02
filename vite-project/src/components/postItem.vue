ee49adfd7da46f1e6765ea93c1c3b4821c1cb57b
DariaG16 <dariagolitsyna16@gmail.com>
2023-06-02 13:41:54 -0400

    Revert "Merge branch 'main' into Dar-dar"
    This reverts commit 0b9d5387a67e363d0241284accb5374b4da8b621, reversing
    changes made to c73c6f51c41753643b4ff7780394f3e977b09f33.

--- vite-project/src/components/postItem.vue
+++ vite-project/src/components/postItem.vue
@@ -12,7 +12,7 @@ const profile = ref(undefined);
 const flipped = ref(false);
 
 async function getProfile() {
-  // console.log(props.post.author);
+  console.log(props.post.author);
   const { data, error } = await supabase
     .from("profiles")
     .select()
@@ -28,69 +28,33 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <div v-if="profile" id="post" @click="flipped = !flipped">
-      <div id="inner" :class="flipped ? 'flipped' : ''">
-        <div id="front">
-          <img v-if="props.post.image" id="postImage" :src="props.post.image" />
-          <img v-else id="postImage" src="noupload.png" />
-          <h3 id="caption">{{ props.post.caption }}</h3>
-          <div id="bottom">
-            <div id="user">
-              <ProfilePicture id="pfp" :src="profile.avatar_url" />
-              <h3 id="signature">{{ profile.username }}</h3>
-            </div>
+  <div v-if="profile" id="post1" @click="flipped = !flipped">
+    <div id="inner" :class="flipped ? 'flipped' : ''">
+      <div id="front">
+        <img v-if="props.post.image" id="postImage" :src="props.post.image" />
+        <img v-else id="postImage" src="noupload.png" />
+        <h3 id="caption">{{ props.post.caption }}</h3>
+        <div id="bottom">
+          <div id="user">
+            <ProfilePicture id="pfp" :src="profile.avatar_url" />
+            <h3 id="signature">{{ profile.username }}</h3>
           </div>
         </div>
-        <div id="back">
-          <h2>{{ props.post.description }}</h2>
-        </div>
+      </div>
+      <div id="back">
+        <h2>{{ props.post.description }}</h2>
       </div>
     </div>
-    <span id="like" class="liked">favorite</span>
   </div>
 </template>
 
 <style scoped>
-
-#like {
-  position: absolute;
-  top: 18vw;
-  left: 9vw;
-  width: 5vw;
-  font-size: 2.2rem;
-  color: red;
-  font-family: 'Material Symbols Outlined';
-  font-weight: normal;
-  font-style: normal;
-  line-height: 1;
-  letter-spacing: normal;
-  text-transform: none;
-  display: inline-block;
-  white-space: nowrap;
-  word-wrap: normal;
-  direction: ltr;
-  -webkit-font-feature-settings: 'liga';
-  -webkit-font-smoodata: antialiased;
-  cursor: pointer;
-}
-
-.liked {
-  font-variation-settings:
-  'FILL' 1,
-  'wght' 400,
-  'GRAD' 0,
-  'opsz' 48
-}
-
-
-#post {
+#post1 {
   width: 13vw;
   position: absolute;
   top: 32%;
   left: 40.5%;
 }
-
 #front,
 #back {
   /* height: 29rem; */
@@ -101,55 +65,45 @@ onMounted(() => {
   background-color: aliceblue;
   box-shadow: 20px 20px 20px rgba(67, 35, 27, 0.379);
   overflow-wrap: break-word;
-  -webkit-backface-visibility: hidden;
-  /* Safari */
+  -webkit-backface-visibility: hidden; /* Safari */
   backface-visibility: hidden;
   padding: 20px;
 }
-
 #back {
   color: black;
   transform: rotateY(180deg);
 }
-
 .flipped {
   transform: rotateY(180deg);
 }
-
 #inner {
   display: flex;
   position: relative;
   transition: transform 0.8s;
   transform-style: preserve-3d;
 }
-
 #postImage {
   width: 100%;
 }
-
 #pfp {
   width: 4rem;
   height: 4rem;
   padding: 15px;
 }
-
 #bottom {
   margin: -23px;
-  display: flex;
 }
-
 #user {
   display: flex;
   flex-direction: row;
   align-items: left;
 }
-
 #signature {
   color: black;
   font-family: "Caveat", cursive;
 }
-
 #caption {
   color: black;
   font-family: "Caveat", cursive;
-}</style>
+}
+</style>
