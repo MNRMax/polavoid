dc0e926553b383cff5d5609f3880553f1ccd90bc
MNRMax <maximilian.shabatayev@gmail.com>
2023-06-03 13:16:45 -0400

    reworked likes and fixed minor stuff

--- vite-project/src/components/postItem.vue
+++ vite-project/src/components/postItem.vue
@@ -9,181 +9,174 @@ const sessionStore = useSessionStore();
 const userID = ref(sessionStore.session.value.user.id);
 
 const props = defineProps({
-  post: Object,
+    post: Object,
 });
 
 const profile = ref(undefined);
 const flipped = ref(false);
+const liked = ref(false);
 
 async function getProfile() {
-  // console.log(props.post.author);
-  const { data, error } = await supabase
-    .from("profiles")
-    .select()
-    .eq("id", props.post.author);
-  profile.value = data[0];
+    // console.log(props.post.author);
+    const { data, error } = await supabase
+        .from("profiles")
+        .select()
+        .eq("id", props.post.author);
+    profile.value = data[0];
 }
 async function handleLike(e) {
-  e.stopPropagation();
-  if (liked.value) {
-    const { error } = await supabase
-      .from("likes")
-      .delete()
-      .match({
-        user_id: sessionStore.session.value.user.id,
-        post_id: props.post.id,
-      });
-  } else {
-    const { error } = await supabase
-      .from("likes")
-      .insert({
-        user_id: sessionStore.session.value.user.id,
-        created_at: new Date(),
-        post_id: props.post.id,
-      });
-  }
-  liked.value = !liked.value;
+    e.stopPropagation();
+    if (liked.value) {
+        const { data, error } = await supabase.rpc('unlike', {
+            postid: props.post.id,
+            userid: sessionStore.session.value.user.id,
+        })
+    } else {
+        const { data, error } = await supabase.rpc('like', {
+            postid: props.post.id,
+            userid: sessionStore.session.value.user.id,
+            date: new Date()
+        })
+    }
+    liked.value = !liked.value;
 }
 onMounted(() => {
-  getProfile();
+    getProfile();
 });
 async function checkLike() {
-  const { data, error } = await supabase.from("likes").select();
-  // .match({ user_id: sessionStore.session.value.user.id, post_id: props.post.id })
-  data.length === 0 ? (liked.value = false) : (liked.value = true);
+    const { data, error } = await supabase.from("likes")
+        .select()
+        .match({ user_id: sessionStore.session.value.user.id, post_id: props.post.id })
+    data.length === 0 ? (liked.value = false) : (liked.value = true);
 }
 checkLike();
 </script>
 
 <template>
-  <div>
-    <div v-if="profile" id="post" @click="flipped = !flipped">
-      <div id="inner" :class="flipped ? 'flipped' : ''">
-        <div id="front">
-          <img v-if="props.post.image" id="postImage" :src="props.post.image" />
-          <img v-else id="postImage" src="noupload.png" />
-          <h3 id="caption">{{ props.post.caption }}</h3>
-          <div id="bottom">
-            <div id="user">
-              <ProfilePicture id="pfp" :src="profile.avatar_url" />
-              <h3 id="signature">{{ profile.username }}</h3>
+    <div>
+        <div v-if="profile" id="post" @click="flipped = !flipped">
+            <div id="inner" :class="flipped ? 'flipped' : ''">
+                <div id="front">
+                    <img v-if="props.post.image" id="postImage" :src="props.post.image" />
+                    <img v-else id="postImage" src="noupload.png" />
+                    <h3 id="caption">{{ props.post.caption }}</h3>
+                    <div id="bottom">
+                        <div id="user">
+                            <ProfilePicture id="pfp" :src="profile.avatar_url" />
+                            <h3 id="signature">{{ profile.username }}</h3>
+                        </div>
+                        <span id="like" :class="liked ? 'liked' : ''" @click="(e) => handleLike(e)">favorite</span>
+                    </div>
+                </div>
+                <div id="back">
+                    <h2>{{ props.post.description }}</h2>
+                </div>
             </div>
-            <span
-              id="like"
-              :class="liked ? 'liked' : ''"
-              @click="(e) => handleLike(e)"
-              >favorite</span
-            >
-          </div>
         </div>
-        <div id="back">
-          <h2>{{ props.post.description }}</h2>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
 #like {
-  position: absolute;
-  top: 18vw;
-  left: 9vw;
-  width: 5vw;
-  font-size: 2.2rem;
-  color: red;
-  font-family: "Material Symbols Outlined";
-  font-weight: normal;
-  font-style: normal;
-  line-height: 1;
-  letter-spacing: normal;
-  text-transform: none;
-  display: inline-block;
-  white-space: nowrap;
-  word-wrap: normal;
-  direction: ltr;
-  -webkit-font-feature-settings: "liga";
-  -webkit-font-smoodata: antialiased;
-  cursor: pointer;
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
+    position: absolute;
+    top: 18vw;
+    left: 9vw;
+    width: 5vw;
+    font-size: 2.2rem;
+    color: red;
+    font-family: "Material Symbols Outlined";
+    font-weight: normal;
+    font-style: normal;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    -webkit-font-feature-settings: "liga";
+    -webkit-font-smoodata: antialiased;
+    cursor: pointer;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
 }
 
 .liked {
-  font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 48;
+    font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 48;
 }
 
 #post {
-  width: 13vw;
-  position: absolute;
-  top: 32%;
-  left: 40.5%;
+    width: 13vw;
+    position: absolute;
+    top: 32%;
+    left: 40.5%;
 }
 
 #front,
 #back {
-  /* height: 29rem; */
-  height: 21vw;
-  position: absolute;
-  margin: auto;
-  width: 100%;
-  background-color: aliceblue;
-  box-shadow: 20px 20px 20px rgba(67, 35, 27, 0.379);
-  overflow-wrap: break-word;
-  -webkit-backface-visibility: hidden;
-  /* Safari */
-  backface-visibility: hidden;
-  padding: 20px;
+    /* height: 29rem; */
+    height: 21vw;
+    position: absolute;
+    margin: auto;
+    width: 100%;
+    background-color: aliceblue;
+    box-shadow: 20px 20px 20px rgba(67, 35, 27, 0.379);
+    overflow-wrap: break-word;
+    -webkit-backface-visibility: hidden;
+    /* Safari */
+    backface-visibility: hidden;
+    padding: 20px;
 }
 
 #back {
-  color: black;
-  transform: rotateY(180deg);
+    color: black;
+    transform: rotateY(180deg);
 }
 
 .flipped {
-  transform: rotateY(180deg);
+    transform: rotateY(180deg);
 }
 
 #inner {
-  display: flex;
-  position: relative;
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
+    display: flex;
+    position: relative;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
 }
 
 #postImage {
-  width: 100%;
+    width: 11vw;
+    height: 14.6vw;
 }
 
 #pfp {
-  width: 4rem;
-  height: 4rem;
-  padding: 15px;
+    width: 4rem;
+    height: 4rem;
+    padding: 15px;
 }
 
 #bottom {
-  margin: -23px;
-  display: flex;
+    margin: -23px;
+    display: flex;
 }
 
 #user {
-  display: flex;
-  flex-direction: row;
-  align-items: left;
+    display: flex;
+    flex-direction: row;
+    align-items: left;
 }
 
 #signature {
-  color: black;
-  font-family: "Caveat", cursive;
+    color: black;
+    font-family: "Caveat", cursive;
 }
 
 #caption {
-  color: black;
-  font-family: "Caveat", cursive;
+    color: black;
+    font-family: "Caveat", cursive;
 }
 </style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -11,79 +11,80 @@ const sessionStore = useSessionStore();
 const post = ref(undefined);
 
 async function getPost() {
-  const { data, error } = await supabase.from("posts").select();
-  return data[0];
+    const { data, error } = await supabase.from("posts").select();
+    return data[0];
 }
 getPost().then((data) => {
-  post.value = data;
-  // console.log(data);
+    post.value = data;
+    // console.log(data);
 });
 </script>
 
 <template>
-  <main class="introPage">
-    <RouterLink
-      v-if="sessionStore.session.value"
-      :to="`/profile/${sessionStore.session.value.user.id}`"
-      >Account</RouterLink
-    >
-    <div class="loginRegister" v-else>
-      <RouterLink id="loginLink" to="/login">Log In</RouterLink>
-      <RouterLink id="registerLink" to="/register">Register</RouterLink>
-    </div>
+    <main class="introPage">
+        <a v-if="sessionStore.session.value" :href="`/profile/${sessionStore.session.value.user.id}`">Account</a>
+        <a v-if="sessionStore.session.value" href="/people">People</a>
+        <a v-if="sessionStore.session.value" href="/post">Create Post</a>
+        <div class="loginRegister" v-else>
+            <a id="loginLink" href="/login">Log In</a>
+            <a id="registerLink" href="/register">Register</a>
+        </div>
 
-    <!-- <WelcomeItem /> -->
-    <div class="intro">
-      <h1>Polavoid</h1>
-      <img src="Blue-light-bulbs.png" alt="string lights" id="stringy" />
-    </div>
-    <div class="fyp">
-      \
-      <StringItem />
-      <!-- <PostItem v-if="post" :post="post" /> -->
-    </div>
-  </main>
+        <!-- <WelcomeItem /> -->
+        <div class="intro">
+            <h1>Polavoid</h1>
+            <img src="Blue-light-bulbs.png" alt="string lights" id="stringy" />
+        </div>
+        <div class="fyp">
+            <StringItem />
+            <!-- <PostItem v-if="post" :post="post" /> -->
+        </div>
+    </main>
 </template>
 
 <style>
 .fyp {
-  display: flex;
-  width: 100%;
-  justify-content: space-around;
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
 }
+
 h1 {
-  font-size: 60px;
-  text-align: center;
-  color: var(--text);
-  background-color: var(--box1);
-  border-radius: 10px;
-  width: 400px;
-  border: solid var(--text) 5px;
-  margin: auto;
+    font-size: 60px;
+    text-align: center;
+    color: var(--text);
+    background-color: var(--box1);
+    border-radius: 10px;
+    width: 400px;
+    border: solid var(--text) 5px;
+    margin: auto;
 }
+
 .intro {
-  height: 400px;
+    height: 400px;
 }
+
 #stringy {
-  z-index: -1;
-  width: 130%;
-  position: absolute;
-  left: -12.5%;
-  top: -190vh;
+    z-index: -1;
+    width: 130%;
+    position: absolute;
+    left: -12.5%;
+    top: -190vh;
 }
+
 .loginRegister {
-  z-index: 4;
-  text-align: center;
-  margin: 30px;
-  font-size: 30px;
+    z-index: 4;
+    text-align: center;
+    margin: 30px;
+    font-size: 30px;
 }
 
 #loginLink,
 #registerLink {
-  background-color: var(--button);
-  border-radius: 10px;
-  border: 3px solid var(--text);
-  margin: 200px;
-  padding: 5px;
+    background-color: var(--button);
+    border-radius: 10px;
+    border: 3px solid var(--text);
+    margin: 200px;
+    padding: 5px;
 }
 </style>
