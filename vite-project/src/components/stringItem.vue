cc1ce898f09bc7d182ad1db19889d28adbf07aa0
melinamui <113132371+melinamui@users.noreply.github.com>
2023-06-04 15:36:25 -0400

    Update stringItem.vue

--- vite-project/src/components/stringItem.vue
+++ vite-project/src/components/stringItem.vue
@@ -2,7 +2,7 @@
 import PostItem from "./postItem.vue";
 import { useSessionStore } from "../stores/session";
 import { supabase } from "../supabase";
-import { ref } from "vue"
+import { ref } from "vue";
 
 const post = ref(undefined);
 
@@ -13,48 +13,26 @@ async function getPost() {
 }
 getPost().then((data) => {
   post.value = data;
-  
-var radius = 100;
-var fields = $('.post'),
-  container = $('#all'),
-  width = container.width(),
-  height = container.height();
-var angle = 0,
-  step = (2 * Math.PI) / fields.length;
-fields.each(function() {
-  var x = Math.round(width / 2 + radius * Math.cos(angle) - $(this).width() / 2);
-  var y = Math.round(height / 2 + radius * Math.sin(angle) - $(this).height() / 2);
-  if (window.console) {
-    console.log($(this).text(), x, y);
-  }
-  $(this).css({
-    left: x + 'px',
-    top: y + 'px'
-  });
-  angle += step;
+  console.log(data);
 });
 
-  console.log(data)
-});
-
-//https://stackoverflow.com/questions/39020670/rotate-objects-around-circle-using-css
-//https://stackoverflow.com/questions/10152390/dynamically-arrange-some-elements-around-a-circle 
 
 
 </script>
 
 <template>
   <div id="all">
-    <PostItem v-if="post" class="post" id="post1" :post="post[0]"/>
-    <PostItem v-if="post" class="post" id="post2" :post="post[1]"/>
-    <PostItem v-if="post" class="post" id="post3" :post="post[2]"/>
+    <PostItem v-if="post" class="post" id="post1" :post="post[0]" />
+    <PostItem v-if="post" class="post" id="post2" :post="post[1]" />
+    <PostItem v-if="post" class="post" id="post3" :post="post[2]" />
   </div>
 </template>
 
 <style scoped>
-#all  {
+#all {
   position: relative;
 }
+
 #post1 {
   position: absolute;
   left: 10%;
@@ -69,4 +47,3 @@ fields.each(function() {
   right: 30%;
 }
 </style>
-
