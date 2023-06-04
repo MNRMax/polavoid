44bdd2ef1a7d0916aa7b3e0b2fc939c652f13337
melinamui <113132371+melinamui@users.noreply.github.com>
2023-06-04 15:25:45 -0400

    foundation for spin thingie

--- vite-project/src/components/stringItem.vue
+++ vite-project/src/components/stringItem.vue
@@ -15,6 +15,26 @@ getPost().then((data) => {
   post.value = data;
   console.log(data)
 });
+
+var radius = 100;
+var fields = $('.post'),
+  container = $('#all'),
+  width = container.width(),
+  height = container.height();
+var angle = 0,
+  step = (2 * Math.PI) / fields.length;
+fields.each(function() {
+  var x = Math.round(width / 2 + radius * Math.cos(angle) - $(this).width() / 2);
+  var y = Math.round(height / 2 + radius * Math.sin(angle) - $(this).height() / 2);
+  if (window.console) {
+    console.log($(this).text(), x, y);
+  }
+  $(this).css({
+    left: x + 'px',
+    top: y + 'px'
+  });
+  angle += step;
+});
 //https://stackoverflow.com/questions/39020670/rotate-objects-around-circle-using-css
 //https://stackoverflow.com/questions/10152390/dynamically-arrange-some-elements-around-a-circle 
 
@@ -23,9 +43,9 @@ getPost().then((data) => {
 
 <template>
   <div id="all">
-    <PostItem v-if="post" id="post1" :post="post[0]"/>
-    <PostItem v-if="post" id="post2" :post="post[1]"/>
-    <PostItem v-if="post" id="post3" :post="post[2]"/>
+    <PostItem v-if="post" class="post" id="post1" :post="post[0]"/>
+    <PostItem v-if="post" class="post" id="post2" :post="post[1]"/>
+    <PostItem v-if="post" class="post" id="post3" :post="post[2]"/>
   </div>
 </template>
 
