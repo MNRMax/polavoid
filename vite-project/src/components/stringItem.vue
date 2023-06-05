84a4e62edfefb8cc7758abfc632ccefdda53cf19
melinamui <113132371+melinamui@users.noreply.github.com>
2023-06-05 13:27:53 -0400

    Update stringItem.vue

--- vite-project/src/components/stringItem.vue
+++ vite-project/src/components/stringItem.vue
@@ -29,21 +29,22 @@ getPost().then((data) => {
 </template>
 
 <style scoped>
-#all {
-  position: relative;
+#all  {
+  /* position: relative; */
 }
-
 #post1 {
   position: absolute;
-  left: 10%;
+  left: 20vw;
+  /* transform-origin: center;
+  transform: rotateZ(180deg); */
 }
 #post2 {
   position: absolute;
-  left: 40%;
+  left: 42.5vw;
   /* transform: translatex(-7.5vw) */
 }
 #post3 {
   position: absolute;
-  right: 30%;
+  right: 33vw;
 }
 </style>
